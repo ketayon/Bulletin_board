96caf2427b4e9a131198b8c9e0f392bf02b375fe
ketayon <ketayon@gmail.com>
2019-09-09 18:59:58 +0300

    Deploy

--- bboard/settings.py
+++ bboard/settings.py
@@ -156,3 +156,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ketayon@gmail.com'
+EMAIL_HOST_PASSWORD = 'KaRaMbA2709'
