eb82e5866b8b685116171833a985411335cab0ee
ketayon <ketayon@gmail.com>
2019-09-12 23:47:57 +0300

    Deploy

--- bboard/settings.py
+++ bboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4&vo9)9ydvrjc*x0t0zt0p&ube_c%m2l4dmzh&_wn-#tz-g(lj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokuapp.com', 'bboard-ketayon.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['bboard-ketayon.herokuapp.com', 'localhost', '127.0.0.1']
 
 
    
