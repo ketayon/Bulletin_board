27999dbce7ab27ccdec6f28fa70b4cf61e4e7bed
ketayon <ketayon@gmail.com>
2019-09-13 14:33:37 +0300

    Deploy

--- bboard/settings.py
+++ bboard/settings.py
@@ -27,10 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['bboard-ketayon.herokuapp.com', 'localhost', '127.0.0.1']
 
-
-   
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -52,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bboard.urls'
