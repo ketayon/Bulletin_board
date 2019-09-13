da5e6b4df8f56889d5f0e57c928fc1573706f132
ketayon <ketayon@gmail.com>
2019-09-13 10:35:41 +0300

    Deploy

--- bboard/settings.py
+++ bboard/settings.py
@@ -115,10 +115,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'main.AdvUser'
 
-import os
-
-EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = 'sendgrid-api-key' # this is your API key
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'ketayon@gmail.com'
 
 
 # Internationalization
