6f7ca190243eaafb24e81019e6a1cbb98bf630be
ketayon <ketayon@gmail.com>
2019-09-14 13:57:19 +0300

    Deploy

--- bboard/urls.py
+++ bboard/urls.py
@@ -26,7 +26,4 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path ('', include('main.urls', namespace='')),
 ]
-if settings.DEBUG:
-    urlpatterns.append(path('static/<path:path>', never_cache(serve)))
-    urlpatterns += static(settings.MEDIA_URL,
-                            document_root=settings.MEDIA_ROOT)
+
